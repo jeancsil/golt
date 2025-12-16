83202759a5c8c8c610ef6010830d4c827c98f48c
Jean Silva <jeancsil@gmail.com>
2025-12-16 21:49:09 +0100

    feat: implement avg, throughput, percentiles calculations

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 )
 
 func main() {
-	// method := flag.String("m", "GET", "HTTP method to use")
 	url := flag.String("u", "http://google.com", "URL to use")
 	requests := flag.Int("n", 10, "Number of requests to make")
 	concurrency := flag.Int("c", 1, "Number of concurrent requests")
@@ -21,6 +20,7 @@ func main() {
 
 	var wg sync.WaitGroup
 	wg.Add(*concurrency)
+	var start = time.Now()
 	for i := 0; i < *concurrency; i++ {
 		go func() {
 			for u := range jobs {
@@ -41,6 +41,31 @@ func main() {
 	close(results)
 
 	timings := make([]time.Duration, 0, *requests)
+	timings = calculateStats(timings, results)
+	reportStats(timings, start)
+}
+
+func reportStats(timings []time.Duration, start time.Time) {
+	n_reqs := len(timings)
+	percentiles := []int{50, 90, 95, 99}
+
+	// fmt.Printf("Timings: %v\n", timings)
+	fmt.Printf("\nFastest: %s\n", timings[0])
+	fmt.Printf("Median: %s\n", timings[n_reqs/2])
+	fmt.Printf("Slowest: %s\n", timings[n_reqs-1])
+	fmt.Printf("Average: %f\n", float64(sliceSum(timings).Seconds())/float64(n_reqs))
+	fmt.Printf("Throughput: %f req/s\n\n", float64(n_reqs)/time.Since(start).Seconds())
+
+	fmt.Println("Percentiles:")
+	length := len(timings)
+	for _, p := range percentiles {
+		index := int(float64(length) * float64(p) / 100)
+		fmt.Printf("%.2f%%: %s\n", float64(p), timings[index])
+	}
+
+}
+
+func calculateStats(timings []time.Duration, results chan time.Duration) []time.Duration {
 	for r := range results {
 		timings = append(timings, r)
 	}
@@ -48,11 +73,8 @@ func main() {
 	sort.Slice(timings, func(i, j int) bool {
 		return timings[i] < timings[j]
 	})
-	fmt.Printf("Timings: %v\n", timings)
-	fmt.Printf("Fastest: %s\n", timings[0])
-	fmt.Printf("Slowest: %s\n", timings[*requests-1])
-	fmt.Printf("Median: %s\n", timings[*requests/2])
 
+	return timings
 }
 
 func makeRequest(url string) time.Duration {
@@ -68,4 +90,12 @@ func makeRequest(url string) time.Duration {
 	defer resp.Body.Close()
 
 	return time.Since(start)
+}
+
+func sliceSum(slice []time.Duration) time.Duration {
+	var total time.Duration
+	for _, t := range slice {
+		total += t
+	}
+	return total
 }
